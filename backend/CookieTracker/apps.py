c1b8d42e14f8ba9332f1821371782206e2c5f475
krasavecotklasa <cegrig4@gmail.com>
2025-03-20 13:50:03 +0200

    init

--- backend/CookieTracker/apps.py
+++ backend/CookieTracker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BackEndConfig(AppConfig):
+class CookietrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'back_end'
+    name = 'CookieTracker'
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u26n)n&=qb!mm!@j2%6)$ai=5r09epcf#p2omq53aqugx57-*!'
+SECRET_KEY = 'django-insecure-@%jm@l2k!qr^d3@wmkmn79c$*3_7(bx-rix$qqhuee3cd(3)2z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    "corsheaders",
-    'back_end',
-    #'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -51,12 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'corsheaders.middleware.CorsMiddleware'
-]
-
-CORS_ALLOWED_ORIGINS = [
-    "https://localhost:3000"
 ]
 
 ROOT_URLCONF = 'backend.urls'
