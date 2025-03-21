8bd2d5b8fcb4f9ea4849452bc4daa577b58ebe80
krasavecotklasa <cegrig4@gmail.com>
2025-03-21 13:38:05 +0200

    forum stuff 2

--- backend/CookieTracker/models.py
+++ backend/CookieTracker/models.py
@@ -3,15 +3,16 @@ from django.conf import settings
 
 # Create your models here.
 
+class Forum(models.Model):
+    name = models.CharField(max_length=2048)
+
 class Post(models.Model):
     poster = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=128)
     text = models.CharField(max_length=3000000)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     poster = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     text = models.CharField(max_length=300000)
-
-class Forum(models.Model):
-    name = models.CharField(max_length=2048)
